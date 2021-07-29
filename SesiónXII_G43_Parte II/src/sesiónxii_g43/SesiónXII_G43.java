e06866a425eead1977cf2b325d8b5a689ed1ac68
YanethM <YanethM@users.noreply.github.com>
2021-07-28 19:43:07 -0500

    Add files via upload

---
+++ SesiónXII_G43_Parte II/src/sesiónxii_g43/SesiónXII_G43.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sesiónxii_g43;
+
+import views_pkg.Principal;
+
+/**
+ *
+ * @author apmejiar
+ */
+public class SesiónXII_G43 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      Principal inst_principal = new Principal();
+      inst_principal.setVisible(true);
+    }
+}
