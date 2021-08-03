12a4251d18454111eee881757972bc628f2fed97
YanethM <YanethM@users.noreply.github.com>
2021-08-02 20:57:53 -0500

    Add files via upload

---
+++ SesiónXV_G43/src/controller_pkg/SesiónXV_G43.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller_pkg;
+
+import model_pkg.ModelDatosDB;
+import model_pkg.Pool;
+import view_pkg.Principal;
+
+/**
+ *
+ * @author apmejiar
+ */
+public class SesiónXV_G43 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ModelDatosDB new_pool = new ModelDatosDB();
+        Principal new_frame = new Principal();
+        new_pool.getProducts(0);
+        new_frame.setVisible(true);
+    }
+    
+}
